d7dc652ab00a2a253ac7466ec7822b16e51a5cf1
taoqibao123 <483822335@qq.com>
2020-08-18 08:46:50 +0800

    错误页面展示

--- vue-template/src/router/index.js
+++ vue-template/src/router/index.js
@@ -76,6 +76,7 @@ export const constantRoutes = [{
     component: () => import('@/views/error-page/401'),
     hidden: true
   },
+  //首页
   {
     path: '/',
     component: Layout,
@@ -97,6 +98,36 @@ export const constantRoutes = [{
   tableRouter,
   excelRouter,
   zipRouter,
+  //错误页面
+  {
+    path: '/error',
+    component: Layout,
+    redirect: 'noRedirect',
+    name: 'ErrorPages',
+    meta: {
+      title: '错误页面',
+      icon: '404'
+    },
+    children: [{
+        path: '401',
+        component: () => import('@/views/error-page/401'),
+        name: 'Page401',
+        meta: {
+          title: '401',
+          noCache: true
+        }
+      },
+      {
+        path: '404',
+        component: () => import('@/views/error-page/404'),
+        name: 'Page404',
+        meta: {
+          title: '404',
+          noCache: true
+        }
+      }
+    ]
+  },
 ]
 
 /**
